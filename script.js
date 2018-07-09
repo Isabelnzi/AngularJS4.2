541f23e7902bc736c53ce75b8c7f017f3828daaa
Nzingani <isabelnzingani@outlook.com>
2018-07-09 11:31:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+ar app = angular.module('testApp', []);
+
+app.controller('marqueCtrl', function($scope) {
+    $scope.marque = [
+		{"marque":"Renault", "couleur":"verte"},
+		{"marque":"Citroen", "couleur":"grise"},
+		{"marque":"Opel", "couleur":"or"},
+		{"marque":"Ferrari", "couleur":"rouge"},
+		{"marque":"Peugeot", "couleur":"bleu"},
+		{"marque":"Nissan", "couleur":"verte"}
+];
+});
